9bd01d34dff508f85bc7110359db921db54d8770
ArvidUnell <arun06001@utb.vaxjo.se>
2023-11-27 14:29:38 +0100

    Add project files.

---
+++ Uppgift_5.A5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift_5_A5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random slump = new Random();
+
+            string[] svar = {"Ja","Nej","Kanske","Typ","Ibland","Sisådar 400","Ja, men faktiskt nej","Varför frågar du mig det?!","Gå hem","Jadu" };
+
+            while (true) 
+            {
+                Console.WriteLine("Ställ en fråga eller \"a\" om du vill avsluta");
+
+                if (Console.ReadLine().ToLower() == "a")
+                {
+                    break;
+                }
+
+                Console.WriteLine(svar[slump.Next(0,11)]);
+            }
+        }
+    }
+}
